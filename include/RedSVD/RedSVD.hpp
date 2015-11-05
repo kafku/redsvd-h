444c274fc2446e8a0f1a5f66cff3edda52fb82ad
kafku <kazunoko93@gmail.com>
2015-11-06 05:00:21 +0900

    fix arg in compute()

--- include/RedSVD/RedSVD.hpp
+++ include/RedSVD/RedSVD.hpp
@@ -269,7 +269,7 @@ namespace RedSVD
 			compute(A, rank);
 		}  
 		
-		void compute(const DenseMatrix& A, const Index rank)
+		void compute(const MatrixType& A, const Index rank)
 		{
 			RedSVD<MatrixType> redsvd(A, rank);
 			
